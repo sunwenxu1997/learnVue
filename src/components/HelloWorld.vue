51ce18c3b633c952a9cde75a8a3fc30e825c494c
sunwenxu1997 <1291397693@qq.com>
2019-08-08 11:16:43 +0800

    修改HelloWorld.vue页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,67 @@
 <template>
   <div class="hello">
-     <img src="../assets/img/logo.png">
+    <img src="../assets/img/logo.png" />
     <h1>{{ msg }}</h1>
     <ul>
-     <li><router-link :to="{name:'drag'}">Tab拖拽</router-link></li>
-      <li><router-link :to="{name:'drag+'}">Tab拖拽(修改+删除)</router-link></li>
-      <li><router-link :to="{name:'zoom'}">图片滚轮缩放</router-link></li>
-       <li><router-link :to="{name:'slot'}">vue插槽</router-link></li>
-        <li><router-link :to="{name:'transition'}">vue渐变动画</router-link></li>
-         <li><router-link :to="{name:'qrcode',query:{id:'12331',code:'FGD1110',phone:'15518270529'}}">vue生成二维码功能</router-link></li>
-           <li><router-link :to="{name:'touch'}">vue-touch左右滑跳转路由</router-link></li>
-            <li><router-link :to="{name:'scaleplate'}">标尺滑动功能</router-link></li>
-             <li><router-link :to="{name:'cropper'}">vue-cropper 头像上传</router-link></li>
-              <li><router-link :to="{name:'DigitalLoad'}">数字加载</router-link></li>
-               <li><router-link :to="{name:'DatePicker'}">日期选择器范围</router-link></li>
-               <li><router-link :to="{name:'share'}">分享功能</router-link></li>
-                <li><router-link :to="{name:'copy'}">一键复制功能</router-link></li>
-                 <li><router-link :to="{name:'InfiniteScroll'}">Element-ui 无限滚动</router-link></li>
-                 <li><router-link :to="{name:'clickButton'}">按键点击效果</router-link></li>
-                 <li><router-link :to="{name:'swiper'}">swiper的使用</router-link></li>
-                  <li><router-link :to="{name:'mock'}">mock.js的使用</router-link></li>
-                   <li><router-link :to="{name:'SearchHistory'}">历史搜索功能</router-link></li>
-                   <li><router-link :to="{name:'DateSchedule'}">日期进度条</router-link></li>
-
+      <li>
+        <router-link :to="{name:'drag'}">Tab拖拽</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'drag+'}">Tab拖拽(修改+删除)</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'zoom'}">图片滚轮缩放</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'slot'}">vue插槽</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'transition'}">vue渐变动画</router-link>
+      </li>
+      <li>
+        <router-link
+          :to="{name:'qrcode',query:{id:'12331',code:'FGD1110',phone:'15518270529'}}"
+        >vue生成二维码功能</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'touch'}">vue-touch左右滑跳转路由</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'scaleplate'}">标尺滑动功能</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'cropper'}">vue-cropper 头像上传</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'DigitalLoad'}">数字加载</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'DatePicker'}">日期选择器范围</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'share'}">分享功能</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'copy'}">一键复制功能</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'InfiniteScroll'}">Element-ui 无限滚动</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'clickButton'}">按键点击效果</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'swiper'}">swiper的使用</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'mock'}">mock.js的使用</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'SearchHistory'}">历史搜索功能</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'DateSchedule'}">日期进度条</router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -32,7 +71,7 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: "Welcome to My Vue.js App"
+      msg: "Welcome to Vue,Hei Hei"
     };
   }
 };
@@ -40,7 +79,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello{
+.hello {
   margin-top: 200px;
   cursor: default;
 }
@@ -54,6 +93,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #338d65;
 }
 </style>
