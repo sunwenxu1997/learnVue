8e9b07994aeb5dda1134c9b0ae6f3742e6c7f0a1
sunwenxu1997 <1291397693@qq.com>
2020-01-06 17:39:11 +0800

    20200106

--- src/components/page/other.vue
+++ src/components/page/other.vue
@@ -1,81 +1,94 @@
 <template>
   <div class="overall">
-    <ul>
-      <li>
-        <section class="nav-z">
-          <span class="nav">
-            <span class="title">HOVER</span>
-          </span>
-        </section>
-        <section class="nav-f">
-          <span class="nav">
-            <span class="title">HOVER</span>
-          </span>
-        </section>
-      </li>
-    </ul>
+    <input type="text" @keyup.enter="enterNum()" v-model="num" />
+    <div class="video-box">
+      <video
+        id="my-video"
+        class="video-js"
+        preload="auto"
+        width="100%"
+        height="100%"
+        data-setup="{}"
+      >
+       <source src="@/assets/mp4/1.mp4" type="video/mp4" />
+        <source src="@/assets/mp4/2.mp4" type="video/mp4" />
+      </video>
+      <!-- <video id="video" width="100%" height="100%" autoplay preload muted>
+       
+      </video> -->
+      <div class="play" @click="clickPlay()"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      num: "",
+      mp4List: []
+    };
+  },
+  mounted() {
+    console.log();
+  },
+  methods: {
+    enterNum() {
+      let _this = this;
+      _this.mp4List = _this.num.split("");
+      console.log(_this.mp4List);
+    },
+    clickPlay() {
+      let video = document.getElementById("my-video");
+      let btn = document.querySelector(".play");
+      btn.style.display = "none";
+      video.play();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-ul {
-  background: rgb(128, 128, 128);
-  list-style: none;
-  padding: 0;
-  li {
-    overflow: hidden;
-    perspective: 500px;
-    cursor: pointer;
-    user-select: none;
-    display: block;
-    width: 150px;
-    height: 60px;
-    color: #808080;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:hover .nav-z {
-      top: -60px;
-      transform: rotateX(90deg);
-    }
-    &:hover .nav-f {
-      z-index: 5;
-      top: 0;
-      transform: rotateX(0deg);
-    }
-    section {
-      transition: 0.3s;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      transform-style: preserve-3d;
-      position: absolute;
-      left: 0;
-      top: 0;
-    }
-    .nav-z {
-      transform-origin: 50% 100%;
-      background: white;
-      top: 0;
-      z-index: 5;
-      color: black;
-      font-weight: bold;
-    }
-    .nav-f {
-      transform-origin: 50% 0%;
-      background: black;
-      transform: rotateX(-90deg);
-      top: 60px;
-      color: white;
-      font-weight: bold;
-    }
-  }
+.video-box {
+  width: 500px;
+  height: 400px;
+  box-shadow: 0 0 5px #ccc;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+video {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.play {
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 0 5px #ccc;
+  border-radius: 50%;
+  position: relative;
+  z-index: 5;
+  cursor: pointer;
+  transition: 0.3s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.play:hover {
+  box-shadow: 0 0 30px #ccc;
+}
+
+.play::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  clip-path: polygon(20% 0, 100% 50%, 20% 100%);
+  background: #5c5c5c;
+  transform: scale(0.4);
 }
 </style>
